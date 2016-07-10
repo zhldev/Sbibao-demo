adf4306b13d88cf9352839aca401aa1998411918
zhl <iwangleung@gmail.com>
2016-07-10 10:23:04 +0800

    Initial Commit

---
+++ Sbibao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sbibao
+//
+//  Created by Zeng on 16/7/10.
+//  Copyright © 2016年 Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
